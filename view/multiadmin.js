d9f33e7da58daae75a6a0dd1d686f8db36e2e335
Vesa Meskanen <vesa.meskanen@logica.com>
2016-03-03 08:07:17 +0200

    Multiphrase view for admin fields

--- view/multiadmin.js
+++ view/multiadmin.js
@@ -1,7 +1,7 @@
 
 /**
   this view is wrapped in a function so it can be re-used for
-  all the admin[property] fields.
+  all the multiadmin[property] fields.
 **/
 
 module.exports = function( property ){
@@ -11,20 +11,18 @@ module.exports = function( property ){
     if( !property ||
         !vs.isset('input:'+property) ||
         !vs.isset('admin:'+property+':analyzer') ||
-        !vs.isset('admin:'+property+':field') ||
+        !vs.isset('multiadmin:'+property+':fields') ||
         !vs.isset('admin:'+property+':boost') ){
       return null;
     }
 
     // base view
-    var view = { "match": {} };
-    
-    // match query
-    view.match[ vs.var('admin:'+property+':field') ] = {
+    var view = { "multi_match": {
       analyzer: vs.var('admin:'+property+':analyzer'),
       boost: vs.var('admin:'+property+':boost'),
+      fields: vs.var('multiadmin:'+property+'fields'),
       query: vs.var('input:'+property)
-    };
+    }};
 
     return view;
   };
