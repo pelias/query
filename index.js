68990673c23507f42d4bae52abe7abe4abef53cc
Vesa Meskanen <vesa.meskanen@logica.com>
2016-04-04 13:56:04 +0300

    Remove yet another deprecated view (multiadmin)

--- index.js
+++ index.js
@@ -15,7 +15,6 @@ module.exports.view = {
   phrase: require('./view/phrase'),
   address: require('./view/address'),
   admin: require('./view/admin'),
-  multiadmin: require('./view/multiadmin'),
   boundary_circle: require('./view/boundary_circle'),
   boundary_rect: require('./view/boundary_rect'),
   boundary_country: require('./view/boundary_country'),
