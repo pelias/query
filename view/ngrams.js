abdaa2aab3f85458f0615aef0441d9ef87680112
Vesa Meskanen <vesa.meskanen@logica.com>
2016-04-04 13:44:07 +0300

    Use variables to select multi matching (no more dedicated multi_match views)
    This makes configuring queries easier, and works automatically with
    layered query configurations such as autocomplete, which customizes
    ngram and phrase views.

--- view/ngrams.js
+++ view/ngrams.js
@@ -4,20 +4,29 @@ module.exports = function( vs ){
   // validate required params
   if( !vs.isset('input:name') ||
       !vs.isset('ngram:analyzer') ||
-      !vs.isset('ngram:field') ||
+      (!vs.isset('ngram:field') && !vs.isset('ngram:multifield')) ||
       !vs.isset('ngram:boost') ){
     return null;
   }
 
-  // base view
-  var view = { "match": {} };
-  
-  // match query
-  view.match[ vs.var('ngram:field') ] = {
-    analyzer: vs.var('ngram:analyzer'),
-    boost: vs.var('ngram:boost'),
-    query: vs.var('input:name')
-  };
+  if(vs.isset('ngram:multifield')) {
+    // multi match query
+    var view = { "multi_match": {
+      analyzer: vs.var('ngram:analyzer'),
+      boost: vs.var('ngram:boost'),
+      query: vs.var('input:name'),
+      fields: vs.var('ngram:multifield')
+    }};
+  }
+  else {
+    var view = { "match": {} };
 
+    // match query
+    view.match[ vs.var('ngram:field') ] = {
+      analyzer: vs.var('ngram:analyzer'),
+      boost: vs.var('ngram:boost'),
+      query: vs.var('input:name')
+    };
+  }
   return view;
 };
