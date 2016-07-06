afe24d2eb192bccae8898e644526c4b58efa9bf4
Alec Vulfson <alec@mapzen.com>
2016-07-06 16:49:23 -0400

    Use terms view in sources view

--- view/sources.js
+++ view/sources.js
@@ -1,11 +1,9 @@
+var terms = require('./terms');
+
 module.exports = function( vs ){
   if( !vs.isset('sources')) {
     return null;
   }
 
-  return {
-    terms: {
-      source: vs.var('sources')
-    }
-  };
+  return terms('source', vs.var('sources'));
 };
