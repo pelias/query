7aeaac247837581a9c55125e1c64ca319ee30ac2
Stephen Hess <stephen.k.hess@gmail.com>
2017-06-29 09:56:24 -0400

    switched to AddressesUsingIdsQuery to class model

--- layout/AddressesUsingIdsQuery.js
+++ layout/AddressesUsingIdsQuery.js
@@ -1,4 +1,7 @@
+'use strict';
+
 const _ = require('lodash');
+const Query = require('./Query');
 
 const baseQuery = {
   query: {
@@ -78,65 +81,59 @@ function createStreetShould(street) {
 
 }
 
-function Layout(){
-  this._score = [];
-  this._filter = [];
+class AddressesUsingIdsQuery extends Query {
+  constructor() {
+    super();
+  }
+
+  render(vs) {
+    const q = _.cloneDeep(baseQuery);
+
+    // add housenumber/street if both are available
+    if (vs.isset('input:housenumber')) {
+      q.query.function_score.query.filtered.query.bool.should.push(
+        createAddressShould(vs.var('input:housenumber'), vs.var('input:street')));
+    }
+
+    // always add street (otherwise this wouldn't be happening)
+    q.query.function_score.query.filtered.query.bool.should.push(
+      createStreetShould(vs.var('input:street')));
+
+    q.size = vs.var('size');
+    q.track_scores = vs.var('track_scores');
+
+    // if there are layer->id mappings, add the layers with non-empty ids
+    if (vs.isset('input:layers')) {
+      const layers_to_ids = JSON.parse(vs.var('input:layers'));
+
+      const shoulds = _.keys(layers_to_ids).reduce((acc, layer) => {
+        if (!_.isEmpty(layers_to_ids[layer])) {
+          acc.push(createShould(layer, layers_to_ids[layer]));
+        }
+        return acc;
+      }, []);
+
+      q.query.function_score.query.filtered.filter.bool.should = shoulds;
+
+    }
+
+    // add all scores
+    q.query.function_score.functions = _.compact(
+      this._score.map((view) => {
+        return view(vs);
+      }
+    ));
+
+    // add all filters
+    q.query.function_score.query.filtered.filter.bool.must = _.compact(
+      this._filter.map((view) => {
+        return view(vs);
+      }
+    ));
+
+    return q;
+  }
+
 }
 
-Layout.prototype.score = function( view ){
-  this._score.push( view );
-  return this;
-};
-
-Layout.prototype.filter = function( view ){
-  this._filter.push( view );
-  return this;
-};
-
-Layout.prototype.render = function( vs ){
-  const q = _.cloneDeep(baseQuery);
-
-  if (vs.isset('input:housenumber')) {
-    q.query.function_score.query.filtered.query.bool.should.push(
-      createAddressShould(vs.var('input:housenumber'), vs.var('input:street')));
-  }
-
-  q.query.function_score.query.filtered.query.bool.should.push(
-    createStreetShould(vs.var('input:street')));
-
-  q.size = vs.var('size');
-  q.track_scores = vs.var('track_scores');
-
-  // if there are layer->id mappings, add the layers with non-empty ids
-  if (vs.isset('input:layers')) {
-    const layers_to_ids = JSON.parse(vs.var('input:layers'));
-
-    const shoulds = _.keys(layers_to_ids).reduce((acc, layer) => {
-      if (!_.isEmpty(layers_to_ids[layer])) {
-        acc.push(createShould(layer, layers_to_ids[layer]));
-      }
-      return acc;
-    }, []);
-
-    q.query.function_score.query.filtered.filter.bool.should = shoulds;
-
-  }
-
-  // add all scores
-  q.query.function_score.functions = _.compact(
-    this._score.map((view) => {
-      return view(vs);
-    }
-  ));
-
-  // add all filters
-  q.query.function_score.query.filtered.filter.bool.must = _.compact(
-    this._filter.map((view) => {
-      return view(vs);
-    }
-  ));
-
-  return q;
-};
-
-module.exports = Layout;
+module.exports = AddressesUsingIdsQuery;
