dc8ac0580bf9472a41d20e1c22e31899d64cdc10
Julian Simioni <julian@simioni.org>
2019-10-03 12:17:54 -0400

    fix(lib): Ensure match and match_phrase handle empty Variables
    We want the match and match_phrase helper functions to gracefully handle
    the case where a variable has no value, which internally is an empty
    string.
    
    This requires doing an explicit comparison to ensure that optional
    parameters are not set unless the variable is actually defined.

--- test/lib/leaf/match_phrase.js
+++ test/lib/leaf/match_phrase.js
@@ -1,4 +1,5 @@
 const match_phrase = require('../../../lib/leaf/match_phrase');
+const Variable = require('../../../lib/Variable');
 
 module.exports.tests = {};
 
@@ -77,6 +78,36 @@ module.exports.tests.match_phrase = function(test, common) {
     };
 
     t.deepEqual(query, expected, 'valid match_phrase query with analyzer');
+    t.end();
+  });
+
+  test('match_phrase query does not allow empty string as optional param value', function(t) {
+    const query = match_phrase('property', 'value', { slop: '' });
+
+    const expected = {
+      match_phrase: {
+        property: {
+          query: 'value'
+        }
+      }
+    };
+
+    t.deepEqual(query, expected, 'valid match_phrase query without empty string value');
+    t.end();
+  });
+
+  test('match_phrase query does not allow empty Variable as optional param value', function(t) {
+    const query = match_phrase('property', 'value', { analyzer: new Variable() });
+
+    const expected = {
+      match_phrase: {
+        property: {
+          query: 'value'
+        }
+      }
+    };
+
+    t.deepEqual(query, expected, 'valid match_phrase query with out empty string value');
     t.end();
   });
 };
